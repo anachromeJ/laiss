f967d2bfeb440d9585e0ceca75e05b0fea2c15a3
Jincheng Li <jincheng_li@brown.edu>
2014-04-05 16:00:01 -0400

    resources

---
+++ php/head.php
@@ -0,0 +1,22 @@
+<head>
+  <meta charset="utf-8">
+  <title>Liberal Arts Initiative Summer School</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css">
+  <link href="Metro-UI-CSS/css/modern.css" rel="stylesheet">
+  <link href="Metro-UI-CSS/css/modern-responsive.css" rel="stylesheet">
+  <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+  <!-- <link rel="stylesheet" href="https://d396qusza40orc.cloudfront.net/startup%2Fcode%2Fsocial-buttons.css"> -->
+  <!-- <script src="https://d396qusza40orc.cloudfront.net/startup%2Fcode%2Fjquery.js"></script> -->
+  <link href="http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    /* Large desktop */
+    @media (min-width: 980px) {
+    body {
+    padding-top: 20px;
+    width: 940px;
+    margin: auto;
+    }
+  </style>
+</head>
